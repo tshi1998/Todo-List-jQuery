53faf08627b3a58f0aaf91d06e2b88cf15df1761
Tony Shi <h58shi@uwaterloo.ca>
2020-10-10 20:29:12 -0400

    Improved UI

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -15,6 +15,10 @@ $("input[type='text'").keypress(function(event){
     if (event.which === 13){
         var todoText = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span> " + todoText +  "</li>");
+        $("ul").append("<li><span><i class = 'fas fa-minus-circle'></i></span> " + todoText +  "</li>");
     }
 })
+
+$(".fa-edit").click(function(){
+    $("input[type='text'").fadeToggle();
+});
